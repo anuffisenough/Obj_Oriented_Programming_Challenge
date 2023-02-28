13992f6b30be827cff9e2b11f7658e8cbc216b33
anuffisenough <uffmanch@gmail.com>
2023-02-28 16:56:34 -0700

    adds initial inquirer funcationality to test

--- Obj_Oriented_Programming/index.js
+++ Obj_Oriented_Programming/index.js
@@ -1,1 +1,28 @@
-console.log("this worked");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Answer this first question",
+            name: "first question",
+        },
+        {
+            type: "input",
+            message: "Answer this second question",
+            name: "second question",
+        },
+        {
+            type: "input",
+            message: "Answer this third question",
+            name: "third question",
+        },
+        {
+            type: "input",
+            message: "Answer this fourth question",
+            name: "fourth question",
+        },
+    ])
+    .then((response) => 
+        response = console.log("this worked")
+        );
