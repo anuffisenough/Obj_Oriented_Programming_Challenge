28f14703d1a4650df1f0cabcbbd491899576eef0
anuffisenough <uffmanch@gmail.com>
2023-04-18 13:41:56 -0600

    cleans up functionality for email clicking

--- Obj_Oriented_Programming/index.js
+++ Obj_Oriented_Programming/index.js
@@ -91,9 +91,9 @@ function askNextQuestion(){
                             fs.appendFile('./dist/teamroster.html', `<div class="card">
                             <div class="card-header">${engineer.getRole()}\n${engineer.EmployeeName}</div>
                               <ul>
-                                <li>${engineer.id}</li>
-                                <li>${engineer.email}</li>
-                                <li>${engineer.getGitHub()}</li>
+                                <li>ID:${engineer.id}</li>
+                                <li>Email:<a href="mailto:${engineer.email}">${engineer.email}</li>
+                                <li>GitHub:<a href="https://github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</li>
                               </ul>
                             </div>`, (err) => (err));
                             askNextQuestion();
@@ -107,9 +107,9 @@ function askNextQuestion(){
                             fs.appendFile('./dist/teamroster.html', `<div class="card">
                             <div class="card-header">${intern.getRole()}\n${intern.EmployeeName}</div>
                               <ul>
-                                <li>${intern.id}</li>
-                                <li>${intern.email}</li>
-                                <li>${intern.getSchool()}</li>
+                                <li>ID:${intern.id}</li>
+                                <li>Email:<a href="mailto:${intern.email}">${intern.email}</li>
+                                <li>School:${intern.getSchool()}</li>
                               </ul>
                             </div>`, (err) => (err));
                             askNextQuestion();
@@ -126,7 +126,7 @@ function askQuestions() {
     inquirer.prompt(managerQuestions)
     .then((response) => {
             const manager = new Manager (response.managerName, response.managerId, response.managerEmailAddress, response.managerOfficeNumber);
-            fs.writeFile('./dist/teamroster.html', `<!DOCTYPE html>\n<html lang="en">\n<head>\n  <meta charset="UTF-8" />\n  <title>Team Roster</title>\n  <link rel="stylesheet" href="stylesheet.css" />\n</head>\n<h1>My Team</h1>\n<div class="flex-container">\n  <div class="card">\n  <div class="card-header">${manager.getRole()}\n${manager.EmployeeName}</div>\n  <ul>\n  <li>${manager.id}</li>\n  <li>${manager.email}</li>\n  <li>${manager.officeNumber}</li>\n</ul>\n</div>\n`, (err) => (err));
+            fs.writeFile('./dist/teamroster.html', `<!DOCTYPE html>\n<html lang="en">\n<head>\n  <meta charset="UTF-8" />\n  <title>Team Roster</title>\n  <link rel="stylesheet" href="stylesheet.css" />\n</head>\n<h1>My Team</h1>\n<div class="flex-container">\n  <div class="card">\n  <div class="card-header">${manager.getRole()}\n${manager.EmployeeName}</div>\n  <ul>\n  <li>ID:${manager.id}</li>\n  <li>Email:<a href="mailto:${manager.email}">${manager.email}</li>\n  <li>${manager.officeNumber}</li>\n</ul>\n</div>\n`, (err) => (err));
 
     inquirer.prompt(doNextQuestion)
     .then((response) => {
@@ -138,11 +138,11 @@ function askQuestions() {
                     const engineer = new Engineer (response.engineerName, response.engineerId, response.engineerEmailAddress, response.engineerGitHub);
                     fs.appendFile('./dist/teamroster.html', `<div class="card">
                     <div class="card-header">${engineer.getRole()}\n${engineer.EmployeeName}</div>
-                      <ul>
-                        <li>${engineer.id}</li>
-                        <li>${engineer.email}</li>
-                        <li>${engineer.getGitHub()}</li>
-                      </ul>
+                    <ul>
+                    <li>ID:${engineer.id}</li>
+                    <li>Email:<a href="mailto:${engineer.email}">${engineer.email}</li>
+                    <li>GitHub:<a href="https://github.com/${engineer.getGitHub()}">${engineer.getGitHub()}</li>
+                  </ul>
                     </div>`, (err) => (err));
                     askNextQuestion();
                 });
@@ -154,11 +154,11 @@ function askQuestions() {
                     const intern = new Intern (response.internName, response.internId, response.internEmailAddress, response.internGitHub);
                     fs.appendFile('./dist/teamroster.html', `<div class="card">
                     <div class="card-header">${intern.getRole()}\n${intern.EmployeeName}</div>
-                      <ul>
-                        <li>${intern.id}</li>
-                        <li>${intern.email}</li>
-                        <li>${intern.getSchool()}</li>
-                      </ul>
+                    <ul>
+                    <li>ID:${intern.id}</li>
+                    <li>Email:<a href="mailto:${intern.email}">${intern.email}</li>
+                    <li>School:${intern.getSchool()}</li>
+                  </ul>
                     </div>`, (err) => (err));
                     askNextQuestion();
                 });
